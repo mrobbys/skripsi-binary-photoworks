d482f0abc3676cf22d04e640485192d44329ac2f
mrobbys <mhmmdrobby48@gmail.com>
2026-06-03 16:12:21 +0800

    chore: install translation pack

---
+++ lang/id/passwords.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'reset'     => 'Kata sandi Anda sudah direset!',
+    'sent'      => 'Link reset password berhasil dikirim!',
+    'throttled' => 'Harap tunggu sebelum mencoba lagi.',
+    'token'     => 'Token pengaturan ulang kata sandi tidak valid.',
+    'user'      => 'Pengguna dengan email tersebut tidak ditemukan.',
+];
