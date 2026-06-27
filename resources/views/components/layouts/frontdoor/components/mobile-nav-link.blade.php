8f11974bc9f95851c0b91dbf65ecc093902d4047
mrobbys <mhmmdrobby48@gmail.com>
2026-06-27 20:59:17 +0800

    refactor(ui): update mobile nav link to use title prop instead of slot

--- resources/views/components/layouts/frontdoor/components/mobile-nav-link.blade.php
+++ resources/views/components/layouts/frontdoor/components/mobile-nav-link.blade.php
@@ -1,12 +1,13 @@
 @props([
     'active' => false,
     'href' => '#',
+    'title' => '',
 ])
 
 <li class="py-4">
   <a href="{{ $href }}"
     class="w-full text-lg {{ $active ? 'font-bold text-stone-900' : 'font-medium text-stone-500' }}"
     aria-current="{{ $active ? 'page' : 'false' }}">
-    {{ $slot }}
+    {{ $title }}
   </a>
 </li>
