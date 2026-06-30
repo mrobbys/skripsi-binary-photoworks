cad286c22f0c9868712b1e1dec5fce5f8014cca7
mrobbys <mhmmdrobby48@gmail.com>
2026-06-30 11:35:07 +0800

    refactor(routes): drop old frontdoor booking index and controller

--- routes/frontdoor/frontdoor.php
+++ routes/frontdoor/frontdoor.php
@@ -1,27 +1,24 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Domains\Frontdoor\Http\Controllers\HomeController;
 use App\Domains\Frontdoor\Http\Controllers\AboutController;
-use App\Domains\Booking\Http\Controllers\Frontdoor\BookingController;
-use App\Domains\Review\Http\Controllers\Frontdoor\ReviewController;
-use App\Domains\Frontdoor\Http\Controllers\FaqController;
 use App\Domains\Frontdoor\Http\Controllers\ContactUsController;
+use App\Domains\Frontdoor\Http\Controllers\FaqController;
+use App\Domains\Frontdoor\Http\Controllers\HomeController;
+use App\Domains\Review\Http\Controllers\Frontdoor\ReviewController;
 use App\Domains\User\Http\Controllers\Frontdoor\DashboardController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('frontdoor.home');
 
 Route::get('/about', [AboutController::class, 'index'])->name('frontdoor.about');
 
-Route::get('/services', [BookingController::class, 'services'])->name('frontdoor.services');
+require __DIR__.'/booking.php';
 
 Route::get('/reviews', [ReviewController::class, 'index'])->name('frontdoor.reviews');
 
 Route::get('/faq', [FaqController::class, 'index'])->name('frontdoor.faq');
 
 Route::get('/contact-us', [ContactUsController::class, 'index'])->name('frontdoor.contact-us');
-
-require __DIR__ . '/booking.php';
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('frontdoor.dashboard.index');
