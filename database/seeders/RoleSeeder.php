1f0ac53174fc7d558cb736031136a857b2c0c47d
mrobbys <mhmmdrobby48@gmail.com>
2026-07-20 19:44:42 +0800

    chore(seeders): update roles and permission seeds

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use App\Domains\User\Enums\RoleType;
-use Spatie\Permission\Models\Permission;
 
 class RoleSeeder extends Seeder
 {
@@ -14,14 +13,69 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        // TODO : tambahkan permission ke masing" role
-        
+
         // * Role superadmin untuk permissionnya didaftarkan di AppServiceProvider
-        
-        // create roles
         $roleSuperadmin = Role::create(['name' => RoleType::SUPERADMIN->value]);
         $roleAdmin = Role::create(['name' => RoleType::ADMIN->value]);
         $roleOwner = Role::create(['name' => RoleType::OWNER->value]);
         $roleUser = Role::create(['name' => RoleType::USER->value]);
+
+        // Memberikan permission ke role admin
+        // Untuk permission kecuali 'system settings'
+        $roleAdmin->givePermissionTo([
+            'dashboard-admin-view',
+
+            'scheduleSession-view',
+            'scheduleSession-update',
+            'scheduleSession-calendar',
+
+            'booking-management-view',
+            'booking-management-create',
+            'booking-management-update',
+            'booking-management-delete',
+
+            'category-master-view',
+            'category-master-create',
+            'category-master-update',
+            'category-master-delete',
+
+            'packageVariant-master-view',
+            'packageVariant-master-create',
+            'packageVariant-master-update',
+            'packageVariant-master-delete',
+
+            'background-master-view',
+            'background-master-create',
+            'background-master-update',
+            'background-master-delete',
+
+            'addon-master-view',
+            'addon-master-create',
+            'addon-master-update',
+            'addon-master-delete',
+
+            'schedule-master-view',
+            'schedule-master-update',
+
+            'review-client-view',
+            'review-client-delete',
+
+            'report-view',
+        ]);
+
+        // Memberikan permission ke role owner
+        // Untuk permission hanya view saja
+        $roleOwner->givePermissionTo([
+            'dashboard-admin-view',
+
+            'scheduleSession-view',
+            'scheduleSession-calendar',
+
+            'booking-management-view',
+
+            'review-client-view',
+
+            'report-view',
+        ]);
     }
 }
