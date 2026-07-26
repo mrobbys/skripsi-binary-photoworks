d6f811c7ca9499d63f84b633837f2b8f918f976c
mrobbys <mhmmdrobby48@gmail.com>
2026-07-26 22:54:00 +0800

    refactor(schedule): sync frontend to data endpoint

--- routes/backdoor/data-master/schedule.php
+++ routes/backdoor/data-master/schedule.php
@@ -7,6 +7,7 @@ Route::prefix('schedule')
   ->name('schedule.')
   ->group(function () {
     Route::get('/', [ScheduleController::class, 'index'])->name('index');
+    Route::get('/data', [ScheduleController::class, 'data'])->name('data');
     Route::patch('/{schedule}', [ScheduleController::class, 'update'])->name('update');
     Route::patch('/{schedule}/toggle', [ScheduleController::class, 'toggleActive'])->name('toggle');
   });
