cb8eaaa60632ce98abb14c6511b48d49b21831c7
mrobbys <mhmmdrobby48@gmail.com>
2026-07-26 22:18:47 +0800

    fix(migration): restrictOnDelete user_id in bookings

--- database/migrations/2026_06_27_210805_create_bookings_table.php
+++ database/migrations/2026_06_27_210805_create_bookings_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->restrictOnDelete();
             $table->foreignId('package_variant_id')->constrained()->restrictOnDelete();
             $table->foreignId('background_id')->nullable()->constrained()->nullOnDelete();
             $table->string('booking_code')->unique();
