16fc634627994f391c66a8d2577ee1b73e51a833
MRobbyS <mhmmdrobby48@gmail.com>
2026-08-23 16:35:40 +0800

    feat(ui): make auth logo clickable to home

--- resources/views/components/auth/logo.blade.php
+++ resources/views/components/auth/logo.blade.php
@@ -1,11 +1,16 @@
 @props([
-  'class' => 'mb-8',
+    'class' => 'mb-8',
 ])
 
 <div class="{{ $class }}">
-  <img
-    src="{{ asset('assets/binary-logo/binary-logo-text-black.png') }}"
-    alt="Binary Photoworks Logo"
-    class="h-12 w-auto object-contain"
+  <a
+    href="{{ route('frontdoor.home') }}"
+    class="inline-block transition-transform duration-300 hover:scale-105"
   >
+    <img
+      src="{{ asset('assets/binary-logo/binary-logo-text-black.png') }}"
+      alt="Binary Photoworks Logo"
+      class="h-12 w-auto object-contain"
+    >
+  </a>
 </div>
